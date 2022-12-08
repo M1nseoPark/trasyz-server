87e4ae55fd5ea14533456ee414615e1e498faba0
M1nseoPark <kjms0820@gmail.com>
2022-12-09 01:26:47 +0900

    GET 요청 받기

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# class Parkinglot(models.Model):
+#     parkinglot_id = models.IntegerField()
+#     pm_id = models.IntegerField()
+#     latitude = models.FloatField()
+#     longitude = models.FloatField()
--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+# http://127.0.0.1:8000/skin/products?id=1&skinType=dry
+# http://127.0.0.1:8000/manager/rent/?pid=1&mid=1&latitude=1&longitude=1
+
+app_name = 'manager'
 urlpatterns = [
     path('', views.index),
+    # http://도메인/manager/?id=123 같은 요청이 들어온 경우 get_post 함수로 파라미터 전달
+    path('rent/', views.get_post),
 ]
