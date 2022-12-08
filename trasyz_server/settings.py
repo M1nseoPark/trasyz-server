9f778690080b63fbf5434a8bd3c6db26a7a48b29
M1nseoPark <kjms0820@gmail.com>
2022-12-08 17:40:50 +0900

    뭐가뭔지 모르겠당

--- trasyz_server/settings.py
+++ trasyz_server/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'manager',
 ]
+
+# REST API에 대한 설정
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',  # admin 사용자만 가능하도록
+    ],
+    'PAGE_SIZE': 5  # pagination 크기 지정 
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
